b1023b2f642e95e7a01dc4a781b07d3bfc202a22
adityaKant <adikant9@gmail.com>
2016-10-21 04:20:53 -0400

    pendingPublishQueue Intializations

--- Xinu/system/topicsInit.c
+++ Xinu/system/topicsInit.c
@@ -3,9 +3,17 @@
 process topicsInit(void){
 	int32 i = 0;
 
+	/* initializing topicTab entries  */
 	for(i = 0; i < NTOPICS; i++){
 		topicEntry *temp = &topicTab[i];
 		temp->nSubscribers = 0;
 		temp->topicSem = semcreate(1);
 	}
+
+	/* initializing pendingPublishCircularQueue */
+	pendingPublishQueue.front = -1;
+	pendingPublishQueue.rear = -1;
+	pendingPublishQueue.mutex = semcreate(1);
+	pendingPublishQueue.emptySlots = semcreate(BUFFER_MAX);
+	pendingPublishQueue.fullSlots = semcreate(0);
 }
