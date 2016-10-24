8376516e7841b1fda86e520f853e854ed90882c0
adityaKant <adikant9@gmail.com>
2016-10-24 01:22:47 -0400

    changes to header files

--- Xinu/include/topics.h
+++ Xinu/include/topics.h
@@ -1,19 +1,20 @@
 #define NTOPICS		256
 #define	NSUBSCRIBERS	8
 
-#define	isbadtopic(x)	( ((topic16)(x) < 0) || \
-			  ((topic16)(x) >= NTOPICS))
+#define	isbadtopic(x)	( ((uint8)(x) < 0) || \
+			  ((uint8)(x) >= NTOPICS))
+#define isbadgroup(x)	( ((uint8)(x) < 0) || \
+			  ((uint8)(x) >= NTOPICS))
 
 typedef struct subscriberEntry{
 	pid32 processId;
 	void (*callback)(topic16,uint32);
+	uint8 groupId;
 }subscriberEntry;
 
 typedef struct topicEntry{
 	subscriberEntry subscribersTab[NSUBSCRIBERS];
 	int16 nSubscribers;
-	// uint32 data;
-	// bool8 dataPublished;
 	sid32 topicSem;
 }topicEntry;
 
