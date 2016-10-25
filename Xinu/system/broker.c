1e811d3fb0f459f5aec74b3b17fbb884d50138b7
adityaKant <adikant9@gmail.com>
2016-10-24 20:27:52 -0400

    broker.c

--- Xinu/system/broker.c
+++ Xinu/system/broker.c
@@ -27,7 +27,7 @@ process broker(){
 
 			for(i = 0; i < fmin(temp->nSubscribers,topicAndData.nSubscribers); i++){
 				if(temp->subscribersTab[i].groupId == *groupNTopicId || *groupNTopicId == 0 || temp->subscribersTab[i].groupId == 0){
-					kprintf("\ncalling handler %d",i);
+					// kprintf("\ncalling handler %d",i);
 					temp->subscribersTab[i].callback(topicAndData.topic,topicAndData.data);
 				}
 			}
